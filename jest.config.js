b7bb32da06beacac26495801cf0d8f46aa5df662
Jay Lee <polyglot.m@gmail.com>
2023-11-02 01:44:03 +0900

    chore(jest.config.js): add jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testRegex: 'src/.*\\.test\\.ts$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,js}'],
+};
