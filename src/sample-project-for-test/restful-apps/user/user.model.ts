bc9fa86221e3613c3c6822539441ffd10f6c8883
Jay Lee <polyglot.m@gmail.com>
2023-11-03 01:42:35 +0900

    feat(user.model): define user.model

---
+++ src/sample-project-for-test/restful-apps/user/user.model.ts
@@ -0,0 +1,30 @@
+import { Model, DataTypes, UUID } from 'sequelize';
+import sequelize from 'src/sample-project-for-test/common/sequelize.util';
+
+class User extends Model {
+  public id!: string;
+  public name?: string;
+  public email?: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+    unique: true,
+  },
+  name: {
+    type: new DataTypes.STRING(128),
+    allowNull: false,
+  },
+  email: {
+    type: new DataTypes.STRING(128),
+  }
+}, {
+  tableName: 'users',
+  sequelize,
+});
+
+export default User;
