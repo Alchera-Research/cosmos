42fe7e39b321f7a9c02b1c410472c130aafa0b2d
Jay Lee <polyglot.m@gmail.com>
2023-11-11 23:38:28 +0900

    config(.eslintrc): add exception rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,7 +76,10 @@ module.exports = {
           {"blankLine": "always", "prev": "*", "next": "if"},
           {"blankLine": "always", "prev": "if", "next": "*"},
           {"blankLine": "always", "prev": "*", "next": "return"}
-        ]
+        ],
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
         // '@typescript-eslint/ban-ts-comment': 'off',
         // '@typescript-eslint/no-explicit-any': 'off',
         // 'jest/no-disabled-tests': 'warn',
