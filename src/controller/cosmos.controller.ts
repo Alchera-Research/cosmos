982161ba376efd1d1c40ced7731aadbdc604c428
Jay Lee <polyglot.m@gmail.com>
2023-11-14 19:02:36 +0900

    feat(cosmos.controller.ts): apply 'ExpressUrlParserLib'

--- src/controller/cosmos.controller.ts
+++ src/controller/cosmos.controller.ts
@@ -1,5 +1,8 @@
+import pluralize from 'pluralize';
 import { Request, Response, NextFunction } from 'express';
+
 import CosmosModelStoreSingleton from 'src/store/cosmos-model-store.singleton';
+import { ExpressUrlParserLib } from '../library/express-url-parser.lib';
 
 const cosmosModelStore = CosmosModelStoreSingleton.getInstance();
 
@@ -12,12 +15,13 @@ class CosmosController {
     req: Request,
     res: Response,
     next: NextFunction,
+    isRestful: boolean = true,
   ): Promise<void> {
-    const targetModel = cosmosModelStore.getSequelizeModel('user');
-    console.log('cosmos.controller.create:c-1:: ', targetModel);
-
+    const parsedUrl = ExpressUrlParserLib.parseUrl(req);
+    const firstLayerModelName = parsedUrl[0].layerName || '';
+    const singulizedModelName: string = pluralize.singular(firstLayerModelName);
+    const targetModel = cosmosModelStore.getSequelizeModel(singulizedModelName);
     const queryResult = await targetModel?.create(req.body);
-    console.log('cosmos.controller.create:c-2:: ', queryResult);
 
     res.status(201).json(queryResult);
   }
