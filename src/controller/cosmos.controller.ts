6b7eff2b9a41ccb3bb12ed28442d6025045c1791
Jay Lee <polyglot.m@gmail.com>
2023-11-14 22:25:58 +0900

    feat(cosmos.controller): implement 'load', and 'get'

--- src/controller/cosmos.controller.ts
+++ src/controller/cosmos.controller.ts
@@ -7,16 +7,47 @@ import { ExpressUrlParserLib } from '../library/express-url-parser.lib';
 const cosmosModelStore = CosmosModelStoreSingleton.getInstance();
 
 class CosmosController {
-  // static async load(controllerParams: ControllerParams) {
-  //   controllerParams?.expressParams?.res.send('cosmos middleware');
-  // }
+  static getModelNameFromUrl(req: Request, res: Response, next: NextFunction) {
+    const parsedUrl = ExpressUrlParserLib.parseUrl(req);
+    const firstLayerModelName = parsedUrl[0].layerName || '';
+    const singulizedModelName: string = pluralize.singular(firstLayerModelName);
+
+    if (!res.locals.cosmos) {
+      res.locals.cosmos = {};
+    }
+
+    res.locals.cosmos.parsedUrl = parsedUrl;
+    res.locals.cosmos.firstLayerModelName = firstLayerModelName;
+    res.locals.cosmos.singulizedModelName = singulizedModelName;
+
+    return singulizedModelName;
+  }
+
+  static getModel(req: Request, res: Response, next: NextFunction): any {
+    const targetModelName = CosmosController.getModelNameFromUrl(req, res, next);
+    const targetModel = cosmosModelStore.getSequelizeModel(targetModelName);
+
+    return targetModel;
+  }
+
+  static async load(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const targetModel = CosmosController.getModel(req, res, next);
+    const targetId = res.locals.cosmos.parsedUrl[0].layerParam;
+    const queryResult = await targetModel?.findByPk(targetId);
+
+    res.locals.cosmos[res.locals.cosmos.singulizedModelName] = queryResult;
+  }
 
   static async create(
     req: Request,
     res: Response,
     next: NextFunction,
     isRestful: boolean = true,
-  ): Promise<void> {
+  ) {
     const parsedUrl = ExpressUrlParserLib.parseUrl(req);
     const firstLayerModelName = parsedUrl[0].layerName || '';
     const singulizedModelName: string = pluralize.singular(firstLayerModelName);
@@ -24,6 +55,17 @@ class CosmosController {
     const queryResult = await targetModel?.create(req.body);
 
     res.status(201).json(queryResult);
+  }
+
+  static async get(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    isRestful: boolean = true,
+  ) {
+    const queryResult = await CosmosController.load(req, res, next);
+
+    res.status(200).json(queryResult);
   }
 }
 
