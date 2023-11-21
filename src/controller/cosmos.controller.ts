20a2a46fd0016b7aa24d50aa83776250c5cf8b8a
Jay Lee <polyglot.m@gmail.com>
2023-11-21 20:50:55 +0900

    feat(consmos.controller.ts): implement bulkCreate

--- src/controller/cosmos.controller.ts
+++ src/controller/cosmos.controller.ts
@@ -27,10 +27,15 @@ class CosmosController {
     const targetModelName = CosmosController.getModelNameFromUrl(req, res, next);
     const targetModel = cosmosModelStore.getSequelizeModel(targetModelName);
 
+    if (!res.locals.cosmos.model) {
+      res.locals.cosmos.model = {};
+    }
+    res.locals.cosmos.model[targetModelName] = targetModel;
+
     return targetModel;
   }
 
-  static async load(
+  static async loadIdData(
     req: Request,
     res: Response,
     next: NextFunction,
@@ -40,9 +45,11 @@ class CosmosController {
     const queryResult = await targetModel?.findByPk(targetId);
 
     res.locals.cosmos[res.locals.cosmos.singulizedModelName] = queryResult;
+
+    return queryResult;
   }
 
-  static async create(
+  static async bulkCreate(
     req: Request,
     res: Response,
     next: NextFunction,
@@ -52,21 +59,97 @@ class CosmosController {
     const firstLayerModelName = parsedUrl[0].layerName || '';
     const singulizedModelName: string = pluralize.singular(firstLayerModelName);
     const targetModel = cosmosModelStore.getSequelizeModel(singulizedModelName);
-    const queryResult = await targetModel?.create(req.body);
+    const queryResult = await targetModel?.bulkCreate(req.body);
 
     res.status(201).json(queryResult);
   }
 
-  static async get(
+  static async create(
     req: Request,
     res: Response,
     next: NextFunction,
     isRestful: boolean = true,
   ) {
-    const queryResult = await CosmosController.load(req, res, next);
+    if (Array.isArray(req.body)) {
+      return CosmosController.bulkCreate(req, res, next, isRestful);
+    }
+
+    const parsedUrl = ExpressUrlParserLib.parseUrl(req);
+    const firstLayerModelName = parsedUrl[0].layerName || '';
+    const singulizedModelName: string = pluralize.singular(firstLayerModelName);
+    const targetModel = cosmosModelStore.getSequelizeModel(singulizedModelName);
+    const queryResult = await targetModel?.create(req.body);
+
+    res.status(201).json(queryResult);
+  }
+
+  static async read(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    isRestful: boolean = true,
+  ) {
+    const queryResult = await CosmosController.loadIdData(req, res, next);
 
     res.status(200).json(queryResult);
   }
+
+  static async update(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    isRestful: boolean = true,
+  ) {
+    const queryResult = await CosmosController.loadIdData(req, res, next) || {};
+    const updateValue = {
+      ...(queryResult.dataValues),
+      ...req.body
+    };
+    const targetModel = res.locals.cosmos.model[res.locals.cosmos.singulizedModelName];
+    console.log('res.locals.cosmos', res.locals.cosmos);
+    console.log('targetModel', targetModel);
+    console.log('updateValue', updateValue);
+    console.log(res.locals.cosmos.parsedUrl[0].layerParam);
+
+    // NOTE: update not working. I don't know why.
+
+    const [affectedRows, numberOfAffectedRows] = await targetModel?.update(
+      updateValue,
+      {
+        where: {
+          id: res.locals.cosmos.parsedUrl[0].layerParam,
+        },
+        returning: ['*'],
+      },
+    );
+
+    let updatedModelQueryResult = affectedRows;
+
+    if (!affectedRows && numberOfAffectedRows) {
+      updatedModelQueryResult = await targetModel?.findByPk(res.locals.cosmos.parsedUrl[0].layerParam);
+    }
+
+    res.status(200).json(updatedModelQueryResult);
+  }
+
+  static async delete(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    isRestful: boolean = true,
+  ) {
+    const queryResult = await CosmosController.loadIdData(req, res, next);
+    const targetModel = res.locals.cosmos.model[res.locals.cosmos.singulizedModelName];
+
+    await targetModel?.destroy({
+      where: {
+        id: res.locals.cosmos.parsedUrl[0].layerParam,
+      },
+    });
+
+    res.status(204).json(queryResult);
+  }
+
 }
 
 export default CosmosController;
